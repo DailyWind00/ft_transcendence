43dc6ed963c0b9749290782b14cb2199682ab1c4
DailyWind00 <dailywind@Gamer>
2024-08-28 07:51:23 +0200

    open Django for database checks

--- BackEnd/backend/settings.py
+++ BackEnd/backend/settings.py
@@ -16,6 +16,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
