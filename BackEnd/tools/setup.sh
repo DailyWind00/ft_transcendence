459f2e18619f5f51f79be1fd414da710532b11ff
DailyWind <dailywind@Transcendence.myguest.virtualbox.org>
2024-08-29 16:02:31 +0200

    added admin django superuser

--- BackEnd/tools/setup.sh
+++ BackEnd/tools/setup.sh
@@ -3,7 +3,7 @@
 # Database configuration
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py createsuperuser --noinput --username=root --email="DailyWind1@gmail.com" 
+echo $DJANGO_SUPERUSER_PASSWORD | python3 manage.py createsuperuser --noinput --username="admin" --email="DailyWind1@gmail.com"
 
 # Run server
 gunicorn --workers=8 --bind 0.0.0.0:2000 backend.wsgi:application
