ddbb4665f20786c3ff592ca578a2aafb75042e94
DailyWind <dailywind@Transcendence.myguest.virtualbox.org>
2024-09-02 10:38:02 +0200

    backend now in https using self-signed certificate

--- BackEnd/backend/settings.py
+++ BackEnd/backend/settings.py
@@ -16,12 +16,15 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 31536000  # 1 year
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-
-
+# HTTPS settings
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
+# ---
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
--- BackEnd/tools/setup.sh
+++ BackEnd/tools/setup.sh
@@ -5,5 +5,6 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 echo $DJANGO_SUPERUSER_PASSWORD | python3 manage.py createsuperuser --noinput --username="admin" --email="DailyWind1@gmail.com"
 
-# Run server
-gunicorn --workers=8 --bind 0.0.0.0:2000 backend.wsgi:application
+# Start server
+# gunicorn --workers=17 --bind 0.0.0.0:2000 "backend.wsgi:application" # HTTP
+gunicorn --workers=17 --bind 0.0.0.0:2000 --certfile "backend.crt" --keyfile "backend.key" "backend.wsgi:application" # HTTPS
