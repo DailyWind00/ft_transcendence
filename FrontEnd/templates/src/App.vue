426505fd75b02be4a5a5ff818253290fbf0aa981
tuvosyl <valentin.soltys@gmail.com>
2024-09-03 14:19:36 +0200

    oui

--- FrontEnd/templates/src/App.vue
+++ FrontEnd/templates/src/App.vue
@@ -7,20 +7,20 @@
     <!-- <div class="background">
       <TestThreeJS></TestThreeJS>
     </div>
+         -->
     <LanguageSelector></LanguageSelector>
-     -->
     <nav id="a">
       <button :class="{'nav-toggle': true, 'is-active': isMenuActive}" @click="toggleMenu">
          ☰
       </button>
       <div id="test" :class="{'nav-menu': true, 'is-active': isMenuActive}" >
-        <router-link id="right" to="/player_info" @click.native="closeMenu">{{ $t('player_info') }}</router-link>
         <router-link id="right" to="/home" @click.native="closeMenu">{{ $t('home') }}</router-link>
-        <router-link id="right" to="/gameselect" @click.native="closeMenu">gameselect</router-link>
-        <router-link id="right" to="/about" @click.native="closeMenu">{{$t('about')}}</router-link>
+        <router-link id="right" to="/gameselect" @click.native="closeMenu">{{ $t('game_choose') }}</router-link>
         <router-link id="right" to="/login" @click.native="closeMenu">{{$t('login')}}</router-link>
         <router-link id="right" to="/register" @click.native="closeMenu">{{$t('sign_up')}}</router-link>
-        <router-link id="right" to="/settings" @click.native="closeMenu">{{$t('setting')}}</router-link>
+        <router-link id="right" to="/player_info" @click.native="closeMenu">{{ $t('player_info') }}</router-link>
+        <router-link id="right" to="/about" @click.native="closeMenu">{{$t('about')}}</router-link>
+        <router-link id="right" to="/settings" @click.native="closeMenu">{{$t('settings')}}</router-link>
       </div>
     </nav>
     <transition name="fade" mode="out-in">
@@ -69,9 +69,6 @@ export default {
   watch: {
     '$i18n.locale': function(newLang) {
       document.documentElement.setAttribute('lang', newLang);
-    },
-    '$route.path': function() {
-      this.updateGlobalData();
     },
     my_BG_color: function(newColor) {
       localStorage.setItem('my_BG_color', newColor);
