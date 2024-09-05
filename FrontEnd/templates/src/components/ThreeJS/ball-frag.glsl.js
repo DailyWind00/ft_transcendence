cf376e4789d0acb0b9a5b0d967bdc3605865bc4f
HaSY xD <anthonyliaudet@gmail.com>
2024-09-05 16:09:48 +0200

    front update

---
+++ FrontEnd/templates/src/components/ThreeJS/ball-frag.glsl.js
@@ -0,0 +1,18 @@
+const ballFrag = `
+varying vec2	vUv;
+varying	vec4	vPosition;
+
+uniform float	time;
+uniform	vec2	resolution;
+
+void	main()
+{
+	vec3	color = vec3(0.1f, 0.0f, 0.15f);
+	
+	if (vPosition.y >= 0.0f)
+		color += vec3(vPosition.y * 4.0f);
+	color.rg *= vUv;
+	gl_FragColor = vec4(color, 1.0f);
+}`;
+
+export default ballFrag;
--- FrontEnd/templates/src/components/ThreeJS/main.js
+++ FrontEnd/templates/src/components/ThreeJS/main.js
@@ -2,9 +2,20 @@ import * as THREE from './three/build/three.module.js'
 
 import cubeVert from './cube-vert.glsl.js';
 import cubeFrag from './cube-frag.glsl.js';
+
 import planeVert from './plane-vert.glsl.js';
 import planeFrag from './plane-frag.glsl.js';
-import postProcess from './postProcess.glsl.js';
+
+import paddleVert from './paddle-vert.glsl.js';
+import paddleFrag from './paddle-frag.glsl.js';
+
+import arenaVert from './arena-vert.glsl.js';
+import arenaFrag from './arena-frag.glsl.js';
+
+import ballVert from './ball-vert.glsl.js';
+import ballFrag from './ball-frag.glsl.js';
+
+const FRAMETIME = 16	//in millisecond
 
 function InitCamera()
 {
@@ -19,14 +30,12 @@ function InitCamera()
 
 function InitRenderer()
 {
-	const renderer = new THREE.WebGLRenderer();
-	const container = document.querySelector(".background");
+	const renderer = new THREE.WebGLRenderer({antialias: true});
 
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	container.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement);
 	return renderer;
 }
-
 
 function InitShader(vertex, fragment)
 {
@@ -50,43 +59,197 @@ function InitPostProcess()
 	return composer;
 }
 
-var camSpeed = -0.02;
+var	camDefaultSpeed = 0.02;
+var	camPanSpeed = 0.2
+var	appState = 'DEFAULT'
+var	rotSpeed = 0.015
+
+function cameraDefaultState()
+{
+	if (camera.rotation.x <= -0.2) {camera.rotation.x += rotSpeed; rotSpeed -= 0.00008} else rotSpeed = 0.015;
+	if (camera.position.z >= 100) {camera.position.z -= camPanSpeed; camPanSpeed -= 0.0005} else camPanSpeed = 0.2;
+	if (camera.position.x >= 150 && camDefaultSpeed > -0.02) camDefaultSpeed -= 0.0001
+	if (camera.position.x <= -150 && camDefaultSpeed < 0.02) camDefaultSpeed += 0.0001
+	if (camera.position.y >= 15) camera.position.y -= camPanSpeed * 0.6;
+	
+	planeMesh.position.x = camera.position.x;
+	arenaMesh.position.x  = camera.position.x;
+	paddleMesh1.position.x = camera.position.x - 28;
+	paddleMesh2.position.x = camera.position.x + 28;
+	ballMesh.position.x = camera.position.x;
+	camera.position.x += camDefaultSpeed;
+}
+
+function cameraGameState()
+{
+	if (camera.rotation.x >= -3.14 / 2) {camera.rotation.x -= rotSpeed; rotSpeed -= 0.00008} else rotSpeed = 0.015;
+	if (camera.position.z <= 125) {camera.position.z += camPanSpeed; camPanSpeed -= 0.0005} else camPanSpeed = 0.2;
+	if (camera.position.y <= 30) camera.position.y += camPanSpeed * 0.6;
+}
+
+function checkCollisionPointRec(point, rec)
+{
+	if (point.position.x > rec.position.x && point.position.x < (rec.position.x - rec.width) &&
+		point.position.z > rec.position.z && point.position.z < (rec.position.z - rec.depth)) {
+		return true;
+	}
+	return false;
+}
+
+var	ballSpeedX = 0.2;
+var	ballSpeedZ = 0.2;
+
+function gameLoop()
+{
+	if (IsKeyDown(KeyS))
+		paddleMesh1.position.z += 0.3;
+	else if (IsKeyDown(KeyW))
+		paddleMesh1.position.z -= 0.3;
+
+	if (paddleMesh1.position.z >= arenaMesh.position.z + 12.5 - 1.75) paddleMesh1.position.z = arenaMesh.position.z + 12.5 - 1.75;
+	if (paddleMesh1.position.z <= arenaMesh.position.z - 12.5 + 1.75) paddleMesh1.position.z = arenaMesh.position.z - 12.5 + 1.75;
+	
+	if (paddleMesh2.position.z >= arenaMesh.position.z + 12.5 - 1.75) paddleMesh2.position.z = arenaMesh.position.z + 12.5 - 1.75;
+	if (paddleMesh2.position.z <= arenaMesh.position.z - 12.5 + 1.75) paddleMesh2.position.z = arenaMesh.position.z - 12.5 + 1.75;
+}
 
 function CubeAnimation()
 {
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	if (camera.position.x >= 150 && camSpeed > -0.02)
-		camSpeed -= 0.005
+	playerInput();
+	if (appState == 'DEFAULT') cameraDefaultState();
+	else if (appState == 'GAME') cameraGameState();
+	else if (appState == 'PLAYING') gameLoop();
 
-	if (camera.position.x <= -150 && camSpeed < 0.02)
-		camSpeed += 0.005
-	// if (camera.position.y < 10.0)
-	// 	camera.position.y += 0.03;
-	// if (camera.position.z > 90.0)
-	// 	camera.position.z -= 0.06;
-	// if (camera.rotation.x >= -3.14 / 2)
-	// 	camera.rotation.x -= 0.005;
+	if (camPanSpeed <= 0.01) camPanSpeed = 0.001;
+	if (rotSpeed <= 0.001) rotSpeed = 0.01;
+
 	groundMesh.material.uniforms.time.value += 1;
 	planeMesh.material.uniforms.time.value += 1;
-	camera.position.x += camSpeed;
-	planeMesh.position.x = camera.position.x
 	renderer.render(scene, camera);
 }
 
+function idMsg(data)
+{
+	;
+}
+
+function startMsg(data)
+{
+	;
+}
+
+function endMsg(data)
+{
+	;
+}
+
+function inGameMsg(data)
+{
+	const plr2yInt = data.charCodeAt(1);
+	const plr2yDec = data.charCodeAt(2);
+	const ballSpeedXInt = data.charCodeAt(3);
+	const ballSpeedXDec = data.charCodeAt(4);
+	const ballSpeedYInt = data.charCodeAt(5);
+	const ballSpeedYDec = data.charCodeAt(6);
+
+	paddleMesh2.position.z = (plr2yInt - 128) + 125;
+	paddleMesh2.position.z += (plr2yDec - 128) / 10;
+	ballMesh.position.x += ballSpeedXInt - 128;
+	ballMesh.position.x += (ballSpeedXDec - 128) / 10;
+	ballMesh.position.z += ballSpeedYInt - 128;
+	ballMesh.position.z += (ballSpeedYDec - 128) / 10;
+}
+
+function scoreMsg(data)
+{
+	const scoringPlayer = data.charCodeAt(1);
+}
+
+function recvFromServ(event)
+{
+	if (!id) {
+		id = event.data.charCodeAt(0);
+		if (id == 2) {
+			paddleMesh2.position.x -= 56;
+			paddleMesh1.position.x += 56;
+		}
+		return ;
+	}
+}
+
+function sendToServ()
+{
+	let	p1PosInt = Math.floor((paddleMesh1.position.z - 125) + 128)
+	let	p1PosDec = Math.floor(10 * ((paddleMesh1.position.z - 125) - Math.floor(paddleMesh1.position.z - 125)) + 128)
+
+	webSocket.send(String.fromCharCode(p1PosInt, p1PosDec));
+}
+
+function playerInput()
+{
+	if (IsKeyPressed(KeySpace)) {
+		if (appState == 'DEFAULT')
+			appState = 'GAME';
+		else if (appState == 'GAME')
+			appState = 'DEFAULT';
+	}
+	if (IsKeyPressed(KeyEnter) && appState == 'GAME') {
+		appState = 'PLAYING';
+		webSocket = new WebSocket("ws://0.0.0.0:8001/");
+		webSocket.addEventListener("message", recvFromServ);
+	}
+
+	if (webSocket)
+		sendToServ();
+}
+
+var id = null;
+var webSocket = null;
 const renderer = InitRenderer();
 const camera = InitCamera();
 const scene = new THREE.Scene();
-//const composer = InitPostProcess();
 
+
+//Meshes initialisation
 const planeGeometry = new THREE.PlaneGeometry(500, 500);
 const planeMesh = new THREE.Mesh(planeGeometry, InitShader(planeVert, planeFrag));
 
-const groundGeometry = new THREE.PlaneGeometry(500, 500, 25, 25);
+const groundGeometry = new THREE.PlaneGeometry(500, 500, 50, 50);
 const groundMesh = new THREE.Mesh(groundGeometry, InitShader(cubeVert, cubeFrag));
 groundMesh.rotation.x = -3.14 / 2;
 
+const arenaGeometry = new THREE.BoxGeometry(68, 1, 25);
+const arenaMesh = new THREE.Mesh(arenaGeometry, InitShader(arenaVert, arenaFrag));
+arenaMesh.position.x = camera.position.x;
+arenaMesh.position.y = 8;
+arenaMesh.position.z = 125;
+
+const paddleGeometry = new THREE.BoxGeometry(0.5, 0.5, 3.5);
+const paddleMesh1 = new THREE.Mesh(paddleGeometry, InitShader(paddleVert, paddleFrag))
+paddleMesh1.position.x = camera.position.x - 28;
+paddleMesh1.position.y = 8.5;
+paddleMesh1.position.z = 125;
+
+const paddleMesh2 = new THREE.Mesh(paddleGeometry, InitShader(paddleVert, paddleFrag))
+paddleMesh2.position.x = camera.position.x + 28;
+paddleMesh2.position.y = 8.5;
+paddleMesh2.position.z = 125;
+
+const ballGeometry = new THREE.SphereGeometry(0.5, 15.0);
+const ballMesh = new THREE.Mesh(ballGeometry, InitShader(ballVert, ballFrag));
+ballMesh.position.x = camera.position.x;
+ballMesh.position.y = 8.5;
+ballMesh.position.z = 125;
+
 scene.add(planeMesh);
 scene.add(groundMesh);
-scene.fog = new THREE.FogExp2(0xaaaaaa, 0.4);
+scene.add(paddleMesh1);
+scene.add(paddleMesh2);
+scene.add(arenaMesh);
+scene.add(ballMesh);
 
-renderer.setAnimationLoop( CubeAnimation );
+InitInputHandling();
+
+setInterval(CubeAnimation, FRAMETIME);
+
+//renderer.setAnimationLoop( CubeAnimation );
