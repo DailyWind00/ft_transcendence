6b08d142c7a5acbf2779f66fc4b872113081851c
Berengere <blebas@localhost.localdomain>
2024-09-07 15:23:41 +0200

    fix error 500 post

--- BackEnd/matchmaking/urls.py
+++ BackEnd/matchmaking/urls.py
@@ -6,3 +6,13 @@ router = DefaultRouter()
 router.register(r'matchmaking', MatchmakingViewSet, basename='matchmaking')
 
 urlpatterns = router.urls
+
+# OU
+
+# from django.urls import path
+# from .views import MatchmakingViewSet
+
+# urlpatterns = [
+#     path('matchmaking/join_queue/', MatchmakingViewSet.as_view({'post': 'join_queue'}), name='join_queue'),
+#     path('matchmaking/get_active_room/', MatchmakingViewSet.as_view({'get': 'get_active_room'}), name='get_active_room'),
+# ]
