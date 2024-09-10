3f8a7708faf81aa6a0d0a5501baa94c223f46679
DailyWind <dailywind@Transcendence.myguest.virtualbox.org>
2024-09-10 12:44:20 +0200

    celery task even set to ON

--- BackEnd/tools/setup.sh
+++ BackEnd/tools/setup.sh
@@ -6,7 +6,7 @@ python3 manage.py migrate
 echo $DJANGO_SUPERUSER_PASSWORD | python3 manage.py createsuperuser --noinput --username="admin" --email="DailyWind1@gmail.com"
 
 # Set up redis
-celery -A backend worker --loglevel=info --uid=celeryuser &
+celery -A backend worker --loglevel=info --uid=celeryuser -E &
 celery -A backend beat --loglevel=info --uid=celeryuser &
 
 # Get Vault token
