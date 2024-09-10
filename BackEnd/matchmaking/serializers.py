31adbadc793360c6c55ba3f9ed5936c349008a84
Berengere <blebas@localhost.localdomain>
2024-09-10 18:40:39 +0200

    remove redis

--- BackEnd/matchmaking/serializers.py
+++ BackEnd/matchmaking/serializers.py
@@ -1,20 +1,11 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Match
 
 class MatchSerializer(serializers.ModelSerializer):
-    player1 = serializers.StringRelatedField()
-    player2 = serializers.StringRelatedField()
+    player1 = serializers.CharField(source='player1.username')
+    player2 = serializers.CharField(source='player2.username')
 
     class Meta:
         model = Match
-        fields = ['id', 'player1', 'player2', 'status', 'created_at']
-
-    def create(self, validated_data):
-        player1_data = validated_data.pop('player1')
-        player2_data = validated_data.pop('player2')
-        
-        player1 = User.objects.get(id=player1_data['id'])
-        player2 = User.objects.get(id=player2_data['id'])
-        
-        match = Match.objects.create(player1=player1, player2=player2, **validated_data)
-        return match
+        fields = ['id', 'player1', 'player2', 'status', 'started_at']
