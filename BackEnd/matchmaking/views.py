1294e772db11f712c5c668a1b0f3383140173bd4
tuvosyl <valentin.soltys@gmail.com>
2024-09-12 04:37:00 +0200

    oui

--- BackEnd/matchmaking/views.py
+++ BackEnd/matchmaking/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .models import PlayerQueue, Match
+from .models import PlayerQueue, Match, first_joined
 from .serializers import MatchSerializer
 from django.db import IntegrityError
 
@@ -32,16 +32,23 @@ def join_matchmaking(request):
 
     queue = PlayerQueue.objects.order_by('joined_at')
 
+    temp_match = Match.objects.filter(player1=player).first()
+    if first_joined.objects.filter(player=player).exists():
+        first_joined.objects.filter(player=player).delete()
+        if temp_match:
+            match_serializer = MatchSerializer(temp_match)
+            return Response(match_serializer.data)
     if queue.count() >= 2:
         player1 = queue[0].player
         player2 = queue[1].player
 
-        if ()
+        
         try:
             match = Match.objects.create(player1=player1, player2=player2, status='in_progress')
         except IntegrityError:
             return Response({'error': 'Failed to create match'}, status=500)
 
+        first = first_joined.objects.create(player=player1)
         PlayerQueue.objects.filter(player=player1).delete()
         PlayerQueue.objects.filter(player=player2).delete()
 
