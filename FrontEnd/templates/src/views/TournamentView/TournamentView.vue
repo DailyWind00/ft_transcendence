c186ceb4a81adf56cce1c0d7ca7b9570fc5f4101
tuvosyl <valentin.soltys@gmail.com>
2024-09-12 11:27:30 +0200

    oui

--- FrontEnd/templates/src/views/TournamentView/TournamentView.vue
+++ FrontEnd/templates/src/views/TournamentView/TournamentView.vue
@@ -41,20 +41,6 @@
 	methods: {
 	  savePlayers() {
 		localStorage.setItem("tournament_players", JSON.stringify(this.players));
-		this.$toast.success("player saved !", {
-		position: "top-center",
-		timeout: 5000,
-		closeOnClick: true,
-		pauseOnFocusLoss: true,
-		pauseOnHover: true,
-		draggable: true,
-		draggablePercent: 0.6,
-		showCloseButtonOnHover: false,
-		hideProgressBar: false,
-		closeButton: "button",
-		icon: true,
-		rtl: false
-		});
 	  },
 	},
   };
